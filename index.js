709c79da6e081fa33e55a4cb0f16ee0f046cbed8
minoj khadgi <minoj_5@hotmail.com>
2018-09-29 16:29:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ function shout(string){
     return string.toLowerCase()
   }
   
-   function logShout (string){
+   function logShout(string){
    return console.log(string.toUpperCase())
   }
-    function logWhisper (string){
+    function logWhisper(string){
    return console.log(string.toLowerCase())
   }
-  function sayHiToGrandma (string){
-  return string.toUpperCase()
+  
+  function sayHiToGrandma(string){
+  return string.toLowerCaseCase()
   }
