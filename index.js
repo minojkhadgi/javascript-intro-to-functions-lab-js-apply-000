d720503ef0d6a2cae27147265922bc324b925414
minoj khadgi <minoj_5@hotmail.com>
2018-09-29 16:58:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ function shout(string){
   }
   
   function sayHiToGrandma(string){
-  return string.toUpperCase()
+   if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
   }
